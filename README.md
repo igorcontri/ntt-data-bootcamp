f50e38e750f646b7388156f2f8825f5a317589b5
Igor Contri <igorcontri@gmail.com>
2024-08-29 15:42:29 -0300

    desafio - criando um sistema bancario com python

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Este repositório tem o propósito de guardar anotações e códigos importantes
 ### Tecnologias e Conceitos
 
 Python
+...
---
+++ sintaxe_basica/02-desafio-sistema-bancario/desafio.py
@@ -0,0 +1,69 @@
+# SISTEMA COM DEPOSITO, SAQUE, EXTRATO
+
+# DEPOISTO -> Não depositar valores negativos, fazer apenas para 1 usuário (não precisa pedir numero de conta, agencia, nome, etc...)
+# SAQUE -> Máximo 3 saques diários, máximo 500,00 por saque, informar o usuário em caso de falta de saldo, todos os saques devem ser armazenados em uma variável e exibidos em uma op de extrato
+# EXTRATO -> Listar todos os depositos e saques realizados na conta, no fim da lista motrar o saldo atual, valores exeibitos no formato R$ 1500.00
+
+menu = """
+
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = "OPERAÇÃO     VALOR\n"
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu).lower()
+
+    if opcao == "d":
+        print("--- DEPÓSITO ---")
+        deposito = float(input("Insira o valor de DEPOSITO => R$ "))
+
+        if deposito > 0:
+            saldo += deposito
+            extrato += "".join(f"Depósito   R$ {deposito:.2f} \n")
+            print(f" R$ {deposito:.2f} Depositados com sucesso!")
+        else:
+            print("Valor de depósito inválido!")
+
+
+    elif opcao == "s":
+        print("--- SAQUE ---")
+        
+        saque = float(input("Insira o valor de SAQUE => R$ "))
+
+        if saque > saldo:
+            print("Erro! Saldo insuficiente!")
+        elif saque > 500:
+            print("Erro! Máximo R$ 500.00 por saque")
+        elif numero_saques >= LIMITE_SAQUES:
+            print("Erro! Limite de 3 saques diários atingido")
+        elif saque > 0:
+            saldo -= saque
+            extrato += f"Saque      R$ {saque:.2f} \n"
+            numero_saques += 1
+            print(f" R$ {saque:.2f} Sacados com sucesso!")
+        else:
+            print("Valor de saque inválido!")
+    
+    elif opcao == "e":
+        print("--- EXTRATO ---")
+        print(extrato)
+        print(f"SALDO ATUAL: R$ {saldo:.2f}")
+        print("---------------")
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Opção inválida, por favor selecione uma opção válida")
+
+    

7a4c08df7429ce079d40c909fe563d1439b0a1f8
Igor Contri <igorcontri@gmail.com>
2024-08-30 08:34:19 -0300

    01_list_comprehension

---
+++ 02_colecoes_python/01-listas/01_list_comprehension.py
@@ -0,0 +1,14 @@
+# Filtro S/ Comprehension
+
+numbers = [1, 30, 21, 2, 9, 65, 34]
+pairs = []
+
+for number in numbers:
+    if number % 2 == 0:
+        pairs.append
+
+# LIST COMPREHENSION
+
+numbers = [1, 30, 21, 2, 9, 65, 34]
+pairs = [number for number in numbers if number % 2 == 0]
+# pairs = [retorno, iteração, condicional (opcional)]

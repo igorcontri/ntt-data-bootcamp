fd817ba00fadeec8fb8873b4c0f0c7728e2c198f
Igor Contri <igorcontri@gmail.com>
2024-08-30 13:42:14 -0300

    01_modulo_datetime

---
+++ 02_colecoes_python/02_datetime/01_modulo_datetime.py
@@ -0,0 +1,19 @@
+from datetime import date, datetime, time
+
+# DATE (y, m, d)
+d = date(2023, 7, 10)
+print(d)
+print(date.today())
+
+# TIME (h, m, s)
+t = time(10, 20, 0)
+print(t)
+
+# DATETIME (y, m, d, h, m, s)
+dt = datetime(2024, 8, 30, 10, 30, 20) # 2024-08-30 10:30:20 
+print(dt)
+print(datetime.today())
+
+
+
+

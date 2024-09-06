259451da15acf6ee8a22dfabd31260d4fa2c9938
Igor Contri <igorcontri@gmail.com>
2024-09-06 10:01:43 -0300

    02_propriedades

--- 03_poo_python/03_encapsulamento/02_propriedades.py
+++ 03_poo_python/03_encapsulamento/02_propriedades.py
@@ -14,3 +14,17 @@ class Pessoa:
     
 pessoa = Pessoa("Igor", 1998)
 print(f"Nome: {pessoa.nome}\tIdade: {pessoa.idade}")
+
+
+# BREVE RESUMO SOBRE DECORATORS
+# Decorators recebem a função abaixo do "@" como argumento, ou seja:
+
+# @property
+# def nome(self):
+
+# É o mesmo que:
+
+# def property(func):
+#   lógica...
+
+# property(nome)
